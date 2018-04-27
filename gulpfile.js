3304921963c42eeb98f47c128e577362b62fea55
Abhijit <abhijitthorat1991@gmail.com>
2018-04-27 17:46:13 +0530

    structure the css by adding some new files. also modify the gulp task to do postcss work

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log("Congratulation!! We have created gulp task");
@@ -10,7 +15,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-	console.log("Imagine Sass and postCSS running here");
+	return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport,nested,cssvars,autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
